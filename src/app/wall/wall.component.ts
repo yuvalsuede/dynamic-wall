34fb3bd1ee92111da24a392c436caf668a7cbba9
Yuval Soueid <yuvalsuede@gmail.com>
2017-06-18 23:21:08 +0300

    wall item

---
+++ src/app/wall/wall.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'wall',
+    template: `
+    
+    `,
+    styles: [`
+    
+    
+    `]
+})
+export class WallComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
